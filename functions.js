656b83b4a5a35347b9e852cc31fa77beb0c0e554
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-07-24 11:16:44 -0400

    Created a generic function example.

---
+++ functions.js
@@ -0,0 +1,10 @@
+var string = "1";
+var addToString = function (str) {
+    string += str;
+    return str;
+};
+console.log(string);
+addToString("2");
+addToString(3);
+addToString(["hi"]);
+console.log(string);
---
+++ functions.ts
@@ -0,0 +1,13 @@
+let string:string = "1"
+
+let addToString = <T>(str: T): T => {
+    string += str
+    return str;
+}
+
+console.log(string)
+addToString("2")
+addToString(3)
+addToString(["hi"])
+console.log(string)
+
